f1692f31d304f0bb3a1da6a5eef68b523eec485d
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-06-21 12:52:32 -0700

    Fetching Orders via Redux

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -36,8 +36,50 @@ export const purchaseBurger = orderData => {
   };
 };
 
+// Set purchased state to false
 export const purchaseInit = () => {
   return {
     type: actionTypes.PURCHASE_INIT
+  };
+};
+
+export const fetchOrdersSuccess = orders => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders
+  };
+};
+
+export const fetchOrdersFail = error => {
+  return {
+    type: actionTypes.FETCH_INGREDIENTS_FAILED,
+    error
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START
+  };
+};
+
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrdersStart());
+    axios
+      .get("/orders.json")
+      .then(({ data }) => {
+        const fetchedOrders = [];
+        for (let key in data) {
+          fetchedOrders.push({
+            ...data[key],
+            id: key
+          });
+        }
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch(err => {
+        dispatch(fetchOrdersFail(err));
+      });
   };
 };
